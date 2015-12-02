b2debd6016935223bc1ba9eb4cf5f1b2e0e6db63
20141105048 <20141105048@sina.com>
2015-12-02 18:38:00 +0800

    Revert "排序"
    This reverts commit b28bf1d88613d7859dd78c054bb8cda9ef0a8f67.

--- sort/main.cpp
+++ sort/main.cpp
@@ -7,30 +7,9 @@
 //
 
 #include <iostream>
-#define N 5
+
 int main(int argc, const char * argv[]) {
     // insert code here...
-    int i,j;
-    int a[N];
-    int temp;
-    
-    for(i=0;i<N;i++)
-        scanf("%d",&a[i]);
-    
-    for(i=0;i<N-1;i++)
-    {
-        for(j=0;j<N-1-i;j++)
-        {
-            if(a[j]>a[j+1])
-            {
-                temp=a[j];
-                a[j]=a[j+1];
-                a[j+1]=temp;
-            }
-        }
-        
-    }
-    for(i=0;i<N;i++)
-        printf("%d\n",a[i]);
+    std::cout << "Hello, World!\n";
     return 0;
 }
